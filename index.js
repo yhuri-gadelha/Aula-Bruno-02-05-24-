c6e81a236cd599e004d7940132d715f9f988513d
yhuri-gadelha <163941414+yhuri-gadelha@users.noreply.github.com>
2024-05-02 20:53:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function preencherEndereco(v) {
+    var cep = v.value;
+
+    cep = cep.replace(/\D/g, '');
+
+    if (cep.length === 8) {
+        var url = 'https://viacep.com.br/ws/'+ cep +'/json/';
+    
+
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                if (!data.erro) {
+                    document.getElementById('idCep').value = data.cep;
+                    document.getElementById('idLogradouro').value = data.logradouro;
+                    document.getElementById('idBairro').value = data.bairro;
+                    document.getElementById('idUf').value = data.localidade+'-'+data.uf;
+                }
+            })
+            .catch(function(error) {
+                console.log('Ocorreu um erro:', error);
+            });
+}
+}
